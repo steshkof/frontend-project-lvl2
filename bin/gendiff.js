32ccdd5b771823d23d856e8f2043911a5309263c
steshkof <steshkof@gmail.com>
2022-05-30 20:38:14 +0300

    step 4.1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,12 +5,12 @@ import { genDiff } from '../src/index.js';
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>')
-    .version('1.0')
-    .option('-f, --format <type>', 'output format')
-    .action((file1, file2) => {
-        console.log(genDiff(file1, file2));
-    });
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .version('1.0')
+  .option('-f, --format <type>', 'output format')
+  .action((file1, file2) => {
+    console.log(genDiff(file1, file2));
+  });
 
 program.parse();
