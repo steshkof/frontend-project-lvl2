69c00bc5c7bdfdea983ea3f1ccda522b15618add
steshkof <steshkof@gmail.com>
2022-05-30 09:43:28 +0300

    step 3.1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { genDiff } from '../src/index.js';
+
 const program = new Command();
 
 program
@@ -7,5 +9,8 @@ program
     .arguments('<filepath1> <filepath2>')
     .version('1.0')
     .option('-f, --format <type>', 'output format')
+    .action((file1, file2) => {
+        console.log(genDiff(file1, file2));
+    });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const parseFile = (file) => {
+    return JSON.parse(fs.readFileSync(file, 'utf-8'))
+}
+
+console.log(process.cwd())
+
+let genDiff = (file1, file2) => {
+    const parsedFile1 = parseFile(file1)
+    const parsedFile2 = parseFile(file2)
+
+    const keysOfFile1 = Object.keys(parsedFile1)
+    const keysOfFile2 = Object.keys(parsedFile2)
+
+    const allKeys = _.sortBy(_.uniq([...keysOfFile1, ...keysOfFile2]));
+
+    let diffResult = "";
+
+    for (let key of allKeys) {
+        const keyInFile1 = Object.hasOwn(parsedFile1, key);
+        const keyInFile2 = Object.hasOwn(parsedFile2, key);
+
+        const value1 = parsedFile1[key];
+        const value2 = parsedFile2[key];
+       
+        // есть в обоих, значения равны
+        if(keyInFile1 && keyInFile2 && value1 === value2) {
+            diffResult += `    ${key}: ${value1}\n`
+        }
+        //есть в обоих, значения разные
+        if(keyInFile1 && keyInFile2 && value1 !== value2) {
+            diffResult += `  - ${key}: ${value1}\n`
+            diffResult += `  + ${key}: ${value2}\n`
+        }
+        // есть только в первом
+        if(keyInFile1 && !keyInFile2) {
+            diffResult += `  - ${key}: ${value1}\n`
+        }
+        //есть только во втором
+        if(!keyInFile1 && keyInFile2) {
+            diffResult += `  + ${key}: ${value2}\n`
+        }
+    }
+    
+    diffResult = `{\n${diffResult}}`
+    return diffResult;
+}
+
+export {genDiff}
