8afc3e2bf8e733d2d27e9411190c2a76c618ef22
steshkof <steshkof@gmail.com>
2022-06-16 17:08:11 +0300

    step 8

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,12 +3,12 @@ import { test, expect } from '@jest/globals';
 import { genDiff } from '../src/index.js';
 import { getFixturePath } from '../src/getPath.js';
 
-// stylish
+// stylish format
 test('test of genDiff JSON, stylish format', () => {
   const fileName1 = 'file1.json';
   const fileName2 = 'file2.json';
 
-  const expected = readFileSync(getFixturePath('expected_json.txt'), 'utf-8');
+  const expected = readFileSync(getFixturePath('expected_stylish.txt'), 'utf-8');
   expect(genDiff(getFixturePath(fileName1), getFixturePath(fileName2), 'stylish')).toEqual(expected);
 });
 
@@ -16,11 +16,11 @@ test('test of genDiff YAML, stylish format', () => {
   const fileName1 = 'file1.yaml';
   const fileName2 = 'file2.yaml';
 
-  const expected = readFileSync(getFixturePath('expected_json.txt'), 'utf-8');
+  const expected = readFileSync(getFixturePath('expected_stylish.txt'), 'utf-8');
   expect(genDiff(getFixturePath(fileName1), getFixturePath(fileName2), 'stylish')).toEqual(expected);
 });
 
-// plain
+// plain format
 test('test of genDiff JSON, plain format', () => {
   const fileName1 = 'file1.json';
   const fileName2 = 'file2.json';
@@ -35,6 +35,23 @@ test('test of genDiff YAML, plain format', () => {
 
   const expected = readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
   expect(genDiff(getFixturePath(fileName1), getFixturePath(fileName2), 'plain')).toEqual(expected);
+});
+
+// json format
+test('test of genDiff JSON, json format', () => {
+  const fileName1 = 'file1.json';
+  const fileName2 = 'file2.json';
+
+  const expected = readFileSync(getFixturePath('expected_json.txt'), 'utf-8');
+  expect(genDiff(getFixturePath(fileName1), getFixturePath(fileName2), 'json')).toEqual(expected);
+});
+
+test('test of genDiff YAML, json format', () => {
+  const fileName1 = 'file1.yaml';
+  const fileName2 = 'file2.yaml';
+
+  const expected = readFileSync(getFixturePath('expected_json.txt'), 'utf-8');
+  expect(genDiff(getFixturePath(fileName1), getFixturePath(fileName2), 'json')).toEqual(expected);
 });
 
 // equal
@@ -52,4 +69,12 @@ test('test of equal files, plain format', () => {
 
   const expected = 'Values are the same';
   expect(genDiff(getFixturePath(fileName1), getFixturePath(fileName2), 'plain')).toEqual(expected);
+});
+
+test('test of equal files, json format', () => {
+  const fileName1 = 'file1.json';
+  const fileName2 = 'file1.json';
+
+  const expected = 'Values are the same';
+  expect(genDiff(getFixturePath(fileName1), getFixturePath(fileName2), 'json')).toEqual(expected);
 });
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,12 @@
 import { formatterStylish } from './stylish.js';
 import { formatterPlain } from './plain.js';
+import { formatterJson } from './json.js';
 
 export default (tree, format) => {
   switch (format) {
     case 'stylish': return formatterStylish(tree);
     case 'plain': return formatterPlain(tree);
+    case 'json': return formatterJson(tree);
     default: return 'Unknown format';
   }
 };
