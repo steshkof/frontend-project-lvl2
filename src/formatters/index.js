a75e56bf2ab0913a016e7e430edb4865f503eade
steshkof <steshkof@gmail.com>
2022-06-27 12:23:18 +0300

    check 2.2

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,12 +2,11 @@ import formatterStylish from './stylish.js';
 import formatterPlain from './plain.js';
 import formatterJson from './json.js';
 
-export default (tree, format) => {
+export default (tree, format = 'stylish') => {
   switch (format) {
     case 'stylish': return formatterStylish(tree);
     case 'plain': return formatterPlain(tree);
     case 'json': return formatterJson(tree);
-    case undefined: return formatterStylish(tree);
     default: return `Unknown format ${format}`;
   }
 };
