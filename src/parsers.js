d055d39f2ff796fffa2502b106bd9aadc2219b75
steshkof <steshkof@gmail.com>
2022-07-08 21:15:15 +0300

    check 4.0

--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,8 @@
 import yaml from 'js-yaml';
 
-const parseFile = (filePath, extension) => {
-  if (extension === '.json') return JSON.parse(filePath);
-  if (extension === '.yaml' || extension === '.yml') return yaml.load(filePath);
+const parseFile = (path, extension) => {
+  if (extension === '.json') return JSON.parse(path);
+  if (extension === '.yaml' || extension === '.yml') return yaml.load(path);
 
   return 'Unrecognized file extension';
 };
